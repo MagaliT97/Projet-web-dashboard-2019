f01b6c1417976c6e16e994346bd80ca0316eca2c
Mathieu Djellali <mathieu.djellali@edu.ece.fr>
2019-03-05 20:45:32 +0100

    ajout formulaire (revenu+deépense)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import NavigationBar from './component/layout/NavigationBar';
 import Home from './component/Home';
-import DashBoard from './component/layout/DashBoard';
 import {BrowserRouter,Route} from 'react-router-dom';
 import FormProject from './component/FormProject';
+import FormDepenses from './component/FormDepenses';
+import FormRevenu from './component/FormRevenu';
 
 class App extends Component {
   render() {
     return (
-     
         <BrowserRouter>
         <div>
             <NavigationBar/>
             <switch>
             <Route exact path='/' component={Home}/>
             <Route path='/FormProject' component={FormProject}/>
+            <Route path='/FormDepenses' component={FormDepenses}/>
+            <Route path='/FormRevenu' component={FormRevenu}/>
             </switch>
         </div>
         </BrowserRouter>
--- src/component/layout/NavigationBar.js
+++ src/component/layout/NavigationBar.js
@@ -2,14 +2,15 @@ import React from 'react'
 import './../../css/NavigationBar.css'
 import './../../css/SideBar.css'
 import SideBar from "../SideBar";
-import {Navbar,Nav,NavDropdown,Button,Form,FormControl} from 'react-bootstrap'
+import {Navbar,Nav,NavDropdown,Button,Form,FormControl,DropdownButton,Dropdown} from 'react-bootstrap'
+import {Link} from 'react-router-dom';
 
 const NavigationBar = () => {
     return(
         <div>
         <SideBar pageWrapId={"page-wrap"}/>
         <Navbar className="nav-bar" collapseOnSelect expand="lg" bg="primary" variant="dark"> 
-        <Navbar.Brand href="#home" className="text-dashboard">DashBoard</Navbar.Brand>
+       <Link to="/" >  <Navbar.Brand href="#home" className="text-dashboard">DashBoard </Navbar.Brand></Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto" >
@@ -18,6 +19,11 @@ const NavigationBar = () => {
                 <Button variant="outline-light">Rechercher</Button>
             </Form>
             </Nav>
+            <DropdownButton id="dropdown-basic-button" title="Formulaire">
+            <Dropdown.Item href="#/action-1"> <Link to="/FormDepenses" className="Formulaire-item">Ajout dépenses</Link></Dropdown.Item>
+            <Dropdown.Item href="#/action-2"> <Link to="/FormRevenu">Ajout revenu </Link> </Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Ajout catégorie</Dropdown.Item>
+            </DropdownButton>
             <i class="fas fa-user fa-2xl"></i>
             <Nav>
             <NavDropdown  alignRight title="Profil" id="collasible-nav-dropdown">
